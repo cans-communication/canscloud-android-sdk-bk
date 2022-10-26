a11b0f595b9b7e3fe71e5e44155dce5526f97c8c
koiMobileDev <koi@canscloud.com>
2022-10-26 16:06:21 +0700

    Done:calling

--- CansCloud-Android-SDK/src/main/java/com/cans/canscloud_android_sdk/CansCenter.kt
+++ CansCloud-Android-SDK/src/main/java/com/cans/canscloud_android_sdk/CansCenter.kt
@@ -12,6 +12,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+@Keep
 object CansCenter {
     fun exToast(context: Context, text : String){
         Toast.makeText(context,text,Toast.LENGTH_SHORT).show()
